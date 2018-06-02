e519567e65c301fa2380dda92aa202ef030f8f5a
Pasquale D'Silva <accounts@psql.me>
2018-06-02 17:41:10 -0400

    Use appropriate rank for 3 dimensional tensor

--- sketch.js
+++ sketch.js
@@ -1,13 +1,13 @@
 function setup () {
 
   const values = [];
-  for (let i = 0; i < 15; i++) {
+  for (let i = 0; i < 30; i++) {
     values[i] = Math.random(0,100);
   }
 
-  const shape = [5,3];
+  const shape = [5,3,2];
 
-  const data = tf.tensor(values, shape);
+  const data = tf.tensor3d(values, shape);
   // (can't reassign a const)
   // const data = tf.tensor([0, 0, 127, 255, 12, 16, 69, 74],[2, 2, 2], 'int32');
 
